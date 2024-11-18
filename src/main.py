e5185b8b3e07dc81377bdf9201203aad23a474b4
Adam Balogh <adambalogh@Adams-MacBook-Air.local>
2024-11-18 14:48:03 +0800

    skeleton

---
+++ src/main.py
@@ -0,0 +1,34 @@
+from twitter_langchain import (
+    TwitterApiWrapper,
+    TwitterToolkit
+)
+
+import os
+import uuid
+
+from langchain_core.messages import HumanMessage
+from langgraph.prebuilt import create_react_agent
+from opengradient.llm import OpenGradientChatModel
+
+# Initialize TwitterApiwrapper
+twitter_api_wrapper = TwitterApiWrapper()
+
+# Create TwitterToolkit from the api wrapper
+twitter_toolkit = TwitterToolkit.from_twitter_api_wrapper(twitter_api_wrapper)
+tools = twitter_toolkit.get_tools()
+
+llm = OpenGradientChatModel(
+    private_key=os.environ.get('PRIVATE_KEY'),
+    model_cid='NousResearch/Hermes-3-Llama-3.1-70B')
+
+# Create agent
+agent_executor = create_react_agent(llm, tools)
+
+# Example - post tweet
+events = agent_executor.stream(
+    {"messages": [("user", "You are degen warren buffett who turned into a crypto bro, mimic his style. Tweet something funny about dogecoin")]},
+    stream_mode="values"
+)
+
+for event in events:
+    event["messages"][-1].pretty_print()
