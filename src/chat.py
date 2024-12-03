3cc60958804768332037570974acfad30119b82f
Adam Balogh <adambalogh@Adams-Air.lan>
2024-12-03 16:05:19 -0500

    twitter

--- src/chat.py
+++ src/chat.py
@@ -6,6 +6,11 @@ from opengradient.llm import OpenGradientChatModel
 from prompts import KAKEGURUI_PROMPT
 import opengradient as og
 
+from twitter_langchain import (
+    TwitterApiWrapper,
+    TwitterToolkit
+)
+
 PRIVATE_KEY = os.environ.get('PRIVATE_KEY')
 if not PRIVATE_KEY:
     raise Exception("Must set PRIVATE_KEY env var")
@@ -17,11 +22,21 @@ messages = [
     {'role': 'user', 'content':  "are you a capitalist?"}
 ]
 
-answer = og.llm_chat(
+hash, stop, response = og.llm_chat(
     model_cid='meta-llama/Llama-3.1-70B-Instruct',
     messages=messages,
     max_tokens=300,
     temperature=0.4
 )
 
-print(answer)
+tweet = response['content']
+print(tweet)
+
+# Initialize TwitterApiwrapper
+twitter_api_wrapper = TwitterApiWrapper()
+
+# Create TwitterToolkit from the api wrapper
+twitter_toolkit = TwitterToolkit.from_twitter_api_wrapper(twitter_api_wrapper)
+tools = twitter_toolkit.get_tools()
+
+tools[2].func(tweet=tweet)
