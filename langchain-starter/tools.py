c34bbe3103fbd62e1a4495984c39d3b2db8d19ff
Adam Balogh <adambalogh@adams-air.mynetworksettings.com>
2025-01-03 15:06:32 -0500

    define tool

--- langchain-starter/tools.py
+++ langchain-starter/tools.py
@@ -6,13 +6,37 @@ from twitter_langchain import (
 )
 from langchain_core.tools import BaseTool
 
-from mltool import create_og_model_tool
+from mltool import create_og_model_tool, ToolType
 
 
 # Define the tools the agent can use
 def create_agent_toolkit() -> List[BaseTool]:
     twitter_api_wrapper = TwitterApiWrapper()
     twitter_toolkit = TwitterToolkit.from_twitter_api_wrapper(twitter_api_wrapper)
+
     tools = twitter_toolkit.get_tools()
+
+    # Add Spot Forecast model
+    tools.append(create_og_model_tool(
+        tool_type=ToolType.LANGCHAIN,
+        model_cid="QmY1RjD3s4XPbSeKi5TqMwbxegumenZ49t2q7TrK7Xdga4",
+        tool_name="SuiSpotForecast",
+        input_getter=lambda: [
+            [1,2,3,4],
+            [1,2,3,4],
+            [1,2,3,4],
+            [1,2,3,4],
+            [1,2,3,4],
+            [1,2,3,4],
+            [1,2,3,4],
+            [1,2,3,4],
+            [1,2,3,4],
+            [1,2,3,4]
+        ],
+        input_schema={
+            "open_high_low_close": List
+        },
+        tool_description="Runs an ML model to forecast the price of SUI 30 minutes from now."
+    ))
 
     return tools
