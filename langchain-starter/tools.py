c413b793dcc56e408aba938590bb628e2f9e364e
Adam Balogh <adambalogh@adams-air.mynetworksettings.com>
2025-01-10 14:10:35 -0500

    data

--- langchain-starter/tools.py
+++ langchain-starter/tools.py
@@ -17,20 +17,24 @@ def create_agent_toolkit() -> List[BaseTool]:
         tool_type=ToolType.LANGCHAIN,
         model_cid="QmY1RjD3s4XPbSeKi5TqMwbxegumenZ49t2q7TrK7Xdga4",
         tool_name="SuiSpotForecast",
-        input_getter=lambda: {"open_high_low_close": [
-            [1,2,3,4],
-            [1,2,3,4],
-            [1,2,3,4],
-            [1,2,3,4],
-            [1,2,3,4],
-            [1,2,3,4],
-            [1,2,3,4],
-            [1,2,3,4],
-            [1,2,3,4],
-            [1,2,3,4]
-        ]},
+        input_getter=lambda: {"open_high_low_close": fetch_sui_price_candles()},
         output_formatter=lambda out: f"The predicted price change is: {out['destandardized_prediction'][0] * 100}%",
         tool_description="Runs an ML model to forecast the price of SUI 30 minutes from now. Requires no input."
     ))
 
     return tools
+
+def fetch_sui_price_candles():
+    # Should replace with real-time data from CEX/DEX
+    return [
+        [1,2,3,4],
+        [1,2,3,4],
+        [1,2,3,4],
+        [1,2,3,4],
+        [1,2,3,4],
+        [1,2,3,4],
+        [1,2,3,4],
+        [1,2,3,4],
+        [1,2,3,4],
+        [1,2,3,4]
+    ]
